3e0646df15ace668c5446ff044c7bb1b07aa942a
safirative <safirative@gmail.com>
2026-01-17 19:00:11 +0700

    Update script.js

--- 01-js-basics/script.js
+++ 01-js-basics/script.js
@@ -3,3 +3,7 @@ const button = document.getElementById("btn");
 button.addEventListener("click", () => {
   console.log("Button clicked");
 });
+
+console.log("Javascript");
+console.log(5);
+console.log(false);
