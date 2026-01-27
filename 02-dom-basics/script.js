21944fe1f47dc5e46529ece6cd3c57deee9ae08b
safirative <safirative@gmail.com>
2026-01-27 20:58:05 +0700

    use innerHTML for dynamic markup

--- 02-dom-basics/script.js
+++ 02-dom-basics/script.js
@@ -6,3 +6,7 @@ title.textContent = "Belajar DOM itu seru";
 //DOM subtitle
 const subtitle = document.getElementById("subtitle");
 subtitle.textContent = "DOM berhasil disentuh";
+
+// learn .textContext vs .innerHTML
+const info = document.getElementById("info");
+info.innerHTML = "<strong>Halo</strong> Dunia";
