766a9079f766e59756808b948512a969ea18d925
safirative <safirative@gmail.com>
2026-01-27 20:43:41 +0700

    Create script.js

---
+++ 02-dom-basics/script.js
@@ -0,0 +1,8 @@
+// DOM title
+const title = document.getElementById("title");
+console.log(title);
+title.textContent = "Belajar DOM itu seru";
+
+//DOM subtitle
+const subtitle = document.getElementById("subtitle");
+subtitle.textContent = "DOM berhasil disentuh";
