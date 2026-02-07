991b6401d2a556c7b44ec59dd4bf422e0a6bab3a
safirative <safirative@gmail.com>
2026-02-07 20:15:26 +0700

    trying forEach

--- 05-array-render/script.js
+++ 05-array-render/script.js
@@ -12,3 +12,13 @@ tasks.forEach((task, index) => {
   item.textContent = index + 1 + ". " + task;
   list.appendChild(item);
 });
+
+// trying other forEach
+const todos = ["Belajar", "Makan", "Tidur"];
+const ctx = { name: "Fira" };
+
+todos.forEach(function(item, index, arr) {
+  console.log(index + 1 + ". " + item);
+  // console.log(arr);
+  console.log(this.name);
+}, ctx);
